bea121b3a77e2906018c0e83125357927b7a4f28
Kai Lüke <kailueke@riseup.net>
2018-05-14 21:41:10 +0900

    lalrpop: update defaults

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate lalrpop;
 use std::process::Command;
 
 fn main() {
-    let s = Command::new("lalrpop").env("LALRPOP_LANE_TABLE", "enabled").arg("src/grammar.lalrpop").status();
+    let s = Command::new("lalrpop").arg("src/grammar.lalrpop").status();
     if s.is_err() {
         lalrpop::process_root().unwrap();
     } else if !s.unwrap().success() {
